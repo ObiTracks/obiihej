f8c4665364ba66783780739a9c8bcb42bf1cf0da
ObiTracks <straightup.stack@gmail.com>
2022-02-20 23:39:55 -0500

    Base templates added and project model

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    class Meta:
+        ordering = ['-date_created']
+    date_created = models.DateTimeField(auto_now=True)  # Autoadd field
+    
